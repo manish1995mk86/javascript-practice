a798fbc76ac0cde3dea67deb83d566cff279a9e5
manish1995mk86 <manish1995mk86@gmail.com>
2024-05-19 15:00:11 +0530

    Add files via upload

---
+++ pizzahut.js
@@ -0,0 +1,78 @@
+console.log("=========Welcome to Manish Pizza Hut=========");
+let pizza=prompt("what size of pizza do you want? Small (S), medium (M) or large (L) :-  ");
+if(pizza=="s")
+{
+    let total=150;
+    let cheese=prompt("do you want extra cheese(Rs.30) ? Y or N :-  ");
+    if(cheese=="y")
+    {
+         total=total+30;
+    }
+    let onion=prompt("do you want extra onion(Rs.30) ? Y or N :-  ");
+    if(onion=="y")
+    {
+         total=total+30;
+    }
+   let tomato=prompt("do you want extra tomato(Rs.30) ? Y or N :-  ");
+    if(tomato=="y")
+    {
+         total=total+30;
+    }
+    let capsicum=prompt("do you want extra capsicum(Rs.30) ? Y or N :-  ");
+    if(capsicum=="y")
+    {
+         total=total+30;
+    }
+    console.log("Your Bill will Be : Rs." +total);
+}
+if(pizza=="m")
+{
+    let total=200;
+    let cheese=prompt("do you want extra cheese(Rs.30) ? Y or N :-  ");
+    if(cheese=="y")
+    {
+         total=total+30;
+    }
+    let onion=prompt("do you want extra onion(Rs.30) ? Y or N :-  ");
+    if(onion=="y")
+    {
+         total=total+30;
+    }
+   let tomato=prompt("do you want extra tomato(Rs.30) ? Y or N :-  ");
+    if(tomato=="y")
+    {
+         total=total+30;
+    }
+    let capsicum=prompt("do you want extra capsicum(Rs.30) ? Y or N :-  ");
+    if(capsicum=="y")
+    {
+         total=total+30;
+    }
+    console.log("Your Bill will Be : Rs." +total);
+}
+if(pizza=="l")
+{
+    let total=250;
+    let cheese=prompt("do you want extra cheese(Rs.30) ? Y or N :-  ");
+    if(cheese=="y")
+    {
+         total=total+30;
+    }
+    let onion=prompt("do you want extra onion(Rs.30) ? Y or N :-  ");
+    if(onion=="y")
+    {
+         total=total+30;
+    }
+   let tomato=prompt("do you want extra tomato(Rs.30) ? Y or N :-  ");
+    if(tomato=="y")
+    {
+         total=total+30;
+    }
+    let capsicum=prompt("do you want extra capsicum(Rs.30) ? Y or N :-  ");
+    if(capsicum=="y")
+    {
+         total=total+30;
+    }
+    console.log("Your Bill will Be : Rs." +total);
+}
+
